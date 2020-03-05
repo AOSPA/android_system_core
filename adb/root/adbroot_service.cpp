omnirom/android_system_core_new

adb_root: Restart adbd when turning off root
 * The running adbd session remains rooted after turning off from UI.

Change-Id: Iedd5282b4e3c11ef6c0959ae6867448521cb2c78

adb/root/adbroot_service.cpp
@@ -16,6 +16,7 @@
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <android-base/strings.h>
 #include <private/android_filesystem_config.h>
 
@@ -61,8 +62,18 @@ binder::Status ADBRootService::setEnabled(bool enabled) {
     }
 
     AutoMutex _l(lock_);
-    enabled_ = enabled;
-    WriteStringToFile(std::to_string(enabled), kStoragePath + kEnabled);
+
+    if (enabled_ != enabled) {
+        enabled_ = enabled;
+        WriteStringToFile(std::to_string(enabled), kStoragePath + kEnabled);
+
+        // Turning off adb root, restart adbd.
+        if (!enabled) {
+            base::SetProperty("lineage.service.adb.root", "0");
+            base::SetProperty("ctl.restart", "adbd");
+        }
+    }
+
     return binder::Status::ok();
 }
 

AOSPA/android_system_core

Signed-off-by: Arian <arian.kulmer@web.de>

adb/root/adbroot_service.cpp
@@ -16,6 +16,7 @@
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
+#include <android-base/properties.h>
 #include <android-base/strings.h>
 #include <private/android_filesystem_config.h>
 
@@ -60,8 +61,18 @@ binder::Status ADBRootService::setEnabled(bool enabled) {
     }
 
     AutoMutex _l(lock_);
-    enabled_ = enabled;
-    WriteStringToFile(std::to_string(enabled), kStoragePath + kEnabled);
+
+    if (enabled_ != enabled) {
+        enabled_ = enabled;
+        WriteStringToFile(std::to_string(enabled), kStoragePath + kEnabled);
+
+        // Turning off adb root, restart adbd.
+        if (!enabled) {
+            base::SetProperty("pa.service.adb.root", "0");
+            base::SetProperty("ctl.restart", "adbd");
+        }
+    }
+
     return binder::Status::ok();
 }
 