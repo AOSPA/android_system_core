4606124176495337592bc189d2be8b2046554283
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:58:40 -0300

    [PixelExperience][eleven] init: Allow forcing custom build fingerprint [2/2]
    Change-Id: Iae1db7ad74d3cb14c2cd4405f7ede13a4444bc5a
    
    Former-commit-id: e139f1e09832c5117cddf551240442a8528c5c99
    Change-Id: I82fff336f94093d3fa332ce9cfdcfb711df0422d

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -802,6 +802,19 @@ static const char *snet_prop_value[] = {
 	NULL
 };
 
+#ifdef TARGET_FORCE_BUILD_FINGERPRINT
+static const char *build_fingerprint_key[] = {
+    "ro.build.fingerprint",
+	"ro.system_ext.build.fingerprint",
+	"ro.vendor.build.fingerprint",
+	"ro.bootimage.build.fingerprint",
+	"ro.odm.build.fingerprint",
+	"ro.product.build.fingerprint",
+	"ro.system.build.fingerprint",
+	NULL
+};
+#endif
+
 static void workaround_snet_properties() {
     // Weaken property override security to set safetynet props
     weaken_prop_override_security = true;
@@ -812,6 +825,12 @@ static void workaround_snet_properties() {
 	for (int i = 0; snet_prop_key[i]; ++i) {
 		InitPropertySet(snet_prop_key[i], snet_prop_value[i]);
 	}
+
+    #ifdef TARGET_FORCE_BUILD_FINGERPRINT
+        for (int i = 0; build_fingerprint_key[i]; ++i) {
+            PropertySet(build_fingerprint_key[i], TARGET_FORCE_BUILD_FINGERPRINT, &error);
+        }
+    #endif
 
     // Restore the normal property override security after safetynet props have been set
     weaken_prop_override_security = false;
@@ -1069,7 +1088,10 @@ void PropertyLoadBootDefaults() {
     vendor_load_properties();
 
     property_initialize_ro_product_props();
+
+#ifndef TARGET_FORCE_BUILD_FINGERPRINT
     property_derive_build_props();
+#endif
 
     update_sys_usb_config();
 
