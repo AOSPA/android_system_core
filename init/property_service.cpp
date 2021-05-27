c0a0889aef899efe8b19a938b0989944dd82ae66
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-27 15:42:25 +0000

    init: set build_tags to release-keys
    * few special apps also care about this alongside safetynet
    
    Change-Id: Ib0ee80918f8ac613e469e7489669ab9269ebcec0
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1292,7 +1292,7 @@ static void SetSafetyNetProps() {
     InitPropertySet("ro.boot.vbmeta.device_state", "locked");
     InitPropertySet("ro.boot.verifiedbootstate", "green");
     InitPropertySet("ro.boot.veritymode", "enforcing");
-
+    InitPropertySet("ro.build.tags", "release-keys");
 }
 
 void PropertyInit() {
